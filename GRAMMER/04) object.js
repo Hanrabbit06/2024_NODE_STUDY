fd2fbeb5574794caae3780757c07e95a3489b78e
Hanrabbit06 <suye8794@gmail.com>
2024-03-18 12:59:41 +0900

    object 속성 추가 및 제거

---
+++ GRAMMER/04) object.js
@@ -0,0 +1,18 @@
+const cathy = {
+    name: "cathy",
+    // 속성 이름은 따옴표 처리 안해도 됨
+    age: 19,
+    skills: ["자바스크립트", "파이썬", "코볼"],
+};
+  
+console.log(cathy);
+console.log(cathy.name);
+console.log(cathy["name"]);
+  
+// 속성 추가
+cathy["취미"] = "독서";
+console.log(cathy);
+  
+// 속성 제거
+delete cathy.취미;
+console.log(cathy);
