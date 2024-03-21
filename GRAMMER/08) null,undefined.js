e9da418129c7414bdf6b8f98acc4b11551031865
Hanrabbit06 <suye8794@gmail.com>
2024-03-21 15:37:59 +0900

    null과 undefined의 비교

---
+++ GRAMMER/08) null,undefined.js
@@ -0,0 +1,13 @@
+let foo;
+console.log(foo);                   //undefined
+
+const odj = {}
+console.log(obj.아무속성)            //undefined
+
+//console.log(bar)                  //에러발생
+
+let bar = null
+console.log(null)                   //null
+
+console.log(null == undefined)      //true
+console.log(null == undefined)      //false
