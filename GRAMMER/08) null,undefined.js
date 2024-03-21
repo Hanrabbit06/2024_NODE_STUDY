bedefa33d44ebaaf0389f18d550a320175596f61
Hanrabbit06 <suye8794@gmail.com>
2024-03-21 15:43:12 +0900

    코멘트 추가

--- GRAMMER/08) null,undefined.js
+++ GRAMMER/08) null,undefined.js
@@ -9,5 +9,6 @@ console.log(obj.아무속성)            //undefined
 let bar = null
 console.log(null)                   //null
 
+//주의할 것
 console.log(null == undefined)      //true
 console.log(null == undefined)      //false
